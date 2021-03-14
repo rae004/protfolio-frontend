404671bd81251326f0be665f6e0c10667894507a
Bob Engel <rae004@gmail.com>
2021-03-14 14:38:20 -0400

    setting lang for accessability in next config instead of index.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     images: {
         domains: ['portfolio-strapi-cdn.s3.us-east-2.amazonaws.com'],
     },
+    i18n: {
+        locales: ['en-US'],
+        defaultLocale: 'en-US'
+    }
 };
--- pages/index.js
+++ pages/index.js
@@ -9,37 +9,35 @@ import Head from "next/head";
 export default function Home({ portfolioItems }) {
     return (
         <Layout>
-            <html lang={"en"}>
-                <Head>
-                    <title>Portfolio</title>
-                    <meta property={"og:title"} content={"Portfolio"} key={"title"}/>
-                    <meta property={"og:description"} content={"Bob's first Next.js App. Using a strapi backend."} key={"description"}/>
-                    <link rel="icon" href={"/myfav.ico"} />
-                </Head>
-                <div key={"top-div"} className="entries">
-                    <div key={"top-div-2"} className="row justify-content-start ">
-                        {portfolioItems.map((portfolio) => (
-                            <div key={"top-div-3"} className="col-md-6">
-                                <div key={"top-div-4"} className="entry mb-3">
-                                    <Link as={"/portfolio/" + portfolio.slug} href="/portfolio/[id]">
-                                        <div key={"top-div-5"} className="main-image">
-                                            <Image
-                                                src={portfolio.image.url}
-                                                width={600}
-                                                height={400}
-                                                alt={portfolio.Headline}
-                                            />
-                                            <a className={'portfolio-post-titles'} href={"/portfolio/" + portfolio.slug}>
-                                                <h1>{portfolio.Headline}</h1>
-                                            </a>
-                                        </div>
-                                    </Link>
-                                </div>
+            <Head>
+                <title>Portfolio</title>
+                <meta property={"og:title"} content={"Portfolio"} key={"title"}/>
+                <meta property={"og:description"} content={"Bob's first Next.js App. Using a strapi backend."} key={"description"}/>
+                <link rel="icon" href={"/myfav.ico"} />
+            </Head>
+            <div key={"top-div"} className="entries">
+                <div key={"top-div-2"} className="row justify-content-start ">
+                    {portfolioItems.map((portfolio) => (
+                        <div key={"top-div-3"} className="col-md-6">
+                            <div key={"top-div-4"} className="entry mb-3">
+                                <Link as={"/portfolio/" + portfolio.slug} href="/portfolio/[id]">
+                                    <div key={"top-div-5"} className="main-image">
+                                        <Image
+                                            src={portfolio.image.url}
+                                            width={600}
+                                            height={400}
+                                            alt={portfolio.Headline}
+                                        />
+                                        <a className={'portfolio-post-titles'} href={"/portfolio/" + portfolio.slug}>
+                                            <h1>{portfolio.Headline}</h1>
+                                        </a>
+                                    </div>
+                                </Link>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
-            </html>
+            </div>
         </Layout>
     );
 }

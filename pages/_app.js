c9deec43e2a3c7e51d270ee9342092e93c6fe30f
Bob Engel <rae004@gmail.com>
2021-03-14 21:17:41 -0400

    changed grid layout from 3 post full width to 2 posts full width, plus added img optimization for pre load and intrinsic layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Added
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </>
-  )
-
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import fetchFromCMS from '../lib/service';
 import Image from 'next/image'
 import Link from 'next/link';
 import Head from "next/head";
-import { Grid, Paper} from '@material-ui/core';
+import { Grid, Paper, CssBaseline } from '@material-ui/core';
 
 //todo create actual home page, move this layout to a new page called /portfolios
 //todo We will have "/" with about purpose and "/portfolios" list collection type portfolio content.
 export default function Home({ portfolioItems }) {
     return (
         <Layout>
+            <CssBaseline/>
             <Head>
                 <title>Portfolio</title>
                 <meta property={"og:title"} content={"Portfolio"} key={"title"}/>
@@ -19,17 +20,24 @@ export default function Home({ portfolioItems }) {
             <div key={"portfolio-root-div"} className={""}>
                 <Grid key={"portfolio-main-grid"} container spacing={3}>
                     {portfolioItems.map((portfolio) => (
-                        <Grid key={portfolio.id} item lg={4}>
+                        <Grid key={portfolio.id} item lg={6}>
                             <Paper>
-                                <Image
-                                    src={portfolio.image.url}
-                                    width={600}
-                                    height={400}
-                                    alt={portfolio.Headline}
-                                />
-                                <a className={'portfolio-post-titles'} href={"/portfolio/" + portfolio.slug}>
-                                    <h1>{portfolio.Headline}</h1>
-                                </a>
+                                <Link href={"/portfolio/" + portfolio.slug}>
+                                    <Image
+                                        src={portfolio.image.url}
+                                        width={600}
+                                        height={400}
+                                        alt={portfolio.Headline}
+                                        priority={true}
+                                        quality={50}
+                                        layout="intrinsic"
+                                    />
+                                </Link>
+                                <h1 className={'portfolio-post-titles'}>
+                                    <Link href={"/portfolio/" + portfolio.slug}>
+                                        {portfolio.Headline}
+                                    </Link>
+                                </h1>
                             </Paper>
                         </Grid>
                     ))}
